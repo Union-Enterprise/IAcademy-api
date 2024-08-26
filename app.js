ce4ff4f0f744727514b2e075f00bef2ecb16e889
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2024-08-26 16:44:18 -0300

    bug fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const testerSeeder = require('./src/seeders/tester').seed;
 
 const routes = require('./routes');
 require('dotenv').config();
