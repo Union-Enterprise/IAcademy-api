bd134cda7af41b9188698231e8262860514e5115
13052006Br <brunosilvadeoliveira.10@gmail.com>
2024-09-09 18:36:31 -0300

    new fields summary users

--- src/models/User.js
+++ src/models/User.js
@@ -34,7 +34,7 @@ const UserSchema = new mongoose.Schema({
     of: String,
     default: {
       "numero": "",
-      "nome": "", 
+      "nome": "",
       "validade": "",
       "cvv": ""
     }
@@ -47,20 +47,20 @@ const UserSchema = new mongoose.Schema({
 
 const UserModel = mongoose.model('User', UserSchema);
 
-class User{
-  constructor(body){
+class User {
+  constructor(body) {
     this.body = body;
     this.errors = [];
     this.user = null;
   }
 
-  passwordCheck(){
+  passwordCheck() {
     const lower = /[a-z]/.test(this.body.password);
     const upper = /[A-Z]/.test(this.body.password);
     const number = /\d/.test(this.body.password);
     const length = (this.body.password).length;
 
-    if(length <= 7 || !lower || !upper || !number) {
+    if (length <= 7 || !lower || !upper || !number) {
       this.errors.push('Insira uma senha com mais de 8 caracteres, entre eles letras minúsculas, maiúsculas e números');
       return false
     } else {
@@ -68,11 +68,11 @@ class User{
     }
   }
 
-  async register(){
+  async register() {
     this.check('signup');
-    if(this.errors.length > 0) return;
+    if (this.errors.length > 0) return;
 
-    if(await this.userAlreadyRegistered(this.body)){
+    if (await this.userAlreadyRegistered(this.body)) {
       this.errors.push('Usuario já existe.');
       return;
     };
@@ -83,59 +83,59 @@ class User{
     return this.user;
   }
 
-  check(type){
-    if(type === 'signup'){
-      if(!this.body.name) this.errors.push('Insira um nome valido.');
+  check(type) {
+    if (type === 'signup') {
+      if (!this.body.name) this.errors.push('Insira um nome valido.');
       this.passwordCheck()
     }
-    if(!validator.isEmail(this.body.email)) this.errors.push('Insira um e-mail valido.');
+    if (!validator.isEmail(this.body.email)) this.errors.push('Insira um e-mail valido.');
   }
-  
-  async userAlreadyRegistered(){
+
+  async userAlreadyRegistered() {
     return await UserModel.findOne({ email: this.body.email });
   }
 
-  async login(type){
-    if(type !== 'auto') this.check();
-    
-    if(this.errors.length > 0) return;
-    try{
+  async login(type) {
+    if (type !== 'auto') this.check();
+
+    if (this.errors.length > 0) return;
+    try {
       this.user = await UserModel.findOne({ email: this.body.email });
 
-      if(this.user === null){
+      if (this.user === null) {
         this.errors.push('O e-mail ou a senha está incorreto.');
         return;
       }
-      if((!this.user.is_adm && this.body.type=="adm") || (this.user.is_adm && !this.body.type)
-         || (!this.user) 
-         || (!bcrypt.compareSync(this.body.password, this.user.password) && this.body.password !== this.user.password)){
+      if ((!this.user.is_adm && this.body.type == "adm") || (this.user.is_adm && !this.body.type)
+        || (!this.user)
+        || (!bcrypt.compareSync(this.body.password, this.user.password) && this.body.password !== this.user.password)) {
         this.errors.push('O e-mail ou a senha está incorreto.');
-        return; 
+        return;
       }
       return this.user;
-    }catch(err){
+    } catch (err) {
       console.log(err);
     }
   }
-  
-  async comparePassword(oldPassword){
-    this.user = await UserModel.findOne({_id: this.body.id}, ["password"])
-    if(!bcrypt.compareSync(oldPassword, this.user.password) && oldPassword !== this.user.password){
+
+  async comparePassword(oldPassword) {
+    this.user = await UserModel.findOne({ _id: this.body.id }, ["password"])
+    if (!bcrypt.compareSync(oldPassword, this.user.password) && oldPassword !== this.user.password) {
       this.errors.push("A senha está incorreta.");
       return false;
-    }else {
+    } else {
       return true;
     }
   }
-  
-  async delete(){
+
+  async delete() {
     const user = await UserModel.findOneAndDelete({ _id: this.body.id, email: this.body.email, name: this.body.name }, ["name", "nickname", "nascimento", "email", "img", "cpf", "links", "is_premium", "createdAt"])
-    
+
     return user;
   }
 
-  async updateName(){
-    if(!this.body.name){
+  async updateName() {
+    if (!this.body.name) {
       this.errors.push("Insira um nome válido");
       return;
     }
@@ -145,18 +145,18 @@ class User{
     return user;
   }
 
-  async updateEmail(){
-    if(!validator.isEmail(this.body.email)){
+  async updateEmail() {
+    if (!validator.isEmail(this.body.email)) {
       this.errors.push("Insira um email válido");
       return;
     }
 
-    if(await this.userAlreadyRegistered(this.body)){
+    if (await this.userAlreadyRegistered(this.body)) {
       this.errors.push("Esse email já está em uso");
       return;
     }
 
-    if(await UserModel.findOne({  _id: this.body.id, googleId: { $exists: true, $ne: "" }})){
+    if (await UserModel.findOne({ _id: this.body.id, googleId: { $exists: true, $ne: "" } })) {
       this.errors.push("Não é possivel alterar email que está vinculado a uma conta Google.");
       return;
     }
@@ -167,15 +167,15 @@ class User{
     return user;
   }
 
-  async updatePassword(){
-    if(!this.body.password){
+  async updatePassword() {
+    if (!this.body.password) {
       this.errors.push("Insira um senha válida");
       return;
     }
-    if(!this.passwordCheck()){
+    if (!this.passwordCheck()) {
       this.errors.push("Insira uma senha com mais de 8 caracteres, entre eles letras minúsculas, maiúsculas e números");
       return;
-    } 
+    }
     const salt = bcrypt.genSaltSync();
     this.body.password = bcrypt.hashSync(this.body.password, salt);
     let user = await UserModel.findOneAndUpdate({ _id: this.body.id }, { password: this.body.password })
@@ -184,13 +184,13 @@ class User{
     return user;
   }
 
-  async updateCPF(){
+  async updateCPF() {
     let user = await UserModel.findOne({ _id: this.body.id });
-    if(user.cpf){
+    if (user.cpf) {
       this.errors.push("CPF não pode ser alterado");
       return;
     }
-    if(!validarCPF(this.body.cpf)){
+    if (!validarCPF(this.body.cpf)) {
       this.errors.push("Insira um cpf válido");
       return;
     }
@@ -200,8 +200,8 @@ class User{
     return user;
   }
 
-  async updateGender(){
-    if(!this.body.genero){
+  async updateGender() {
+    if (!this.body.genero) {
       this.errors.push("Insira um gênero");
       return;
     }
@@ -211,22 +211,22 @@ class User{
     return user;
   }
 
-  async updateAddress(){
-    if(!this.body.cep){
+  async updateAddress() {
+    if (!this.body.cep) {
       this.errors.push("Insira um endereço");
       return;
     }
     let user = await UserModel.findOneAndUpdate({ _id: this.body.id }, { cep: this.body.cep, rua: this.body.rua, numero: this.body.numero, bairro: this.body.bairro, cidade: this.body.cidade, estado: this.body.estado })
-    if(this.body.complemento!=""){
-      user = await UserModel.findOneAndUpdate({ _id: this.body.id }, {complemento: this.body.complemento});
+    if (this.body.complemento != "") {
+      user = await UserModel.findOneAndUpdate({ _id: this.body.id }, { complemento: this.body.complemento });
     }
     user = await UserModel.findOne({ _id: this.body.id }, ["name", "nickname", "nascimento", "email", "img", "cpf", "links", "is_premium", "createdAt", "cep", "rua", "numero", "bairro", "cidade", "complemento", "estado"]);
 
     return user;
   }
 
-  async updateBirth(){
-    if(!this.body.nascimento){
+  async updateBirth() {
+    if (!this.body.nascimento) {
       this.errors.push("Insira uma data de nascimento");
       return;
     }
@@ -236,8 +236,8 @@ class User{
     return user;
   }
 
-  async updatePhone(){
-    if(!this.body.telefone){
+  async updatePhone() {
+    if (!this.body.telefone) {
       this.errors.push("Insira um número de telefone");
       return;
     }
@@ -247,18 +247,18 @@ class User{
     return user;
   }
 
-  async updateIMG(){
-    let user = await UserModel.findOneAndUpdate({ _id: this.body.id }, { img: `http://localhost:5002/files/`+this.body.img })
+  async updateIMG() {
+    let user = await UserModel.findOneAndUpdate({ _id: this.body.id }, { img: `http://localhost:5002/files/` + this.body.img })
     user = await UserModel.findOne({ _id: this.body.id }, ["name", "nickname", "nascimento", "email", "img", "cpf", "links", "is_premium", "createdAt"]);
 
     return user;
   }
 
-  async getUser(){
+  async getUser() {
     return await UserModel.findOne({ _id: this.body.id }, ["name", "nickname", "email", "googleId", "nascimento", "telefone", "img", "cpf", "genero", "links", "is_premium", "cep", "rua", "bairro", "cidade", "numero", "complemento", "estado", "is_adm", "createdAt"]);
   }
 
-  async forgotPassword(email, token, now){
+  async forgotPassword(email, token, now) {
     const user = await UserModel.findOne({ email });
 
     await UserModel.findOneAndUpdate({ email }, { passwordResetToken: token, passwordResetExpires: now })
@@ -268,22 +268,22 @@ class User{
     return user;
   }
 
-  async verifyToken(token){
+  async verifyToken(token) {
     this.user = await UserModel.findOne({ email: this.body.email });
 
-    if(!this.user){
+    if (!this.user) {
       this.errors.push('Email não encontrado');
       return;
     }
 
-    if(token !== this.user.passwordResetToken){
+    if (token !== this.user.passwordResetToken) {
       this.errors.push('Token invalido')
       return;
     }
 
     const now = new Date();
 
-    if(now > this.user.passwordResetExpires){
+    if (now > this.user.passwordResetExpires) {
       this.errors.push('Token expirado, gere um novo.');
       return;
     }
@@ -291,35 +291,35 @@ class User{
     return true;
   }
 
-  async resetPassword(token, password){
+  async resetPassword(token, password) {
     this.user = await UserModel.findOne({ email: this.body.email });
 
-    if(!this.user){
+    if (!this.user) {
       this.errors.push('Email não encontrado');
       return;
     }
 
-    if(token !== this.user.passwordResetToken){
+    if (token !== this.user.passwordResetToken) {
       this.errors.push('Token invalido')
       return;
     }
 
     const now = new Date();
 
-    if(now > this.user.passwordResetExpires){
+    if (now > this.user.passwordResetExpires) {
       this.errors.push('Token expirado, gere um novo.');
       return;
     }
 
-    if(!password){
+    if (!password) {
       this.errors.push("Insira um senha válida");
       return;
     }
     this.body.password = password;
-    if(!this.passwordCheck()){
+    if (!this.passwordCheck()) {
       this.errors.push("Insira uma senha com mais de 8 caracteres, entre eles letras minúsculas, maiúsculas e números");
       return;
-    } 
+    }
     const salt = bcrypt.genSaltSync();
     this.body.password = bcrypt.hashSync(password, salt);
 
@@ -331,18 +331,18 @@ class User{
     return user;
   }
 
-  async getRecentUsersADM(qtd){
+  async getRecentUsersADM(qtd) {
     try {
       let recentUsers;
-      if(qtd){
-          recentUsers = await UserModel.find({$or: [{ is_adm: { $exists: false } }, { is_adm: false }] })
+      if (qtd) {
+        recentUsers = await UserModel.find({ $or: [{ is_adm: { $exists: false } }, { is_adm: false }] })
           .sort({ createdAt: -1 })
           .limit(qtd)
-          .select('name nickname email img is_adm is_premium is_banned'); 
-      }else{
-          recentUsers = await UserModel.find({$or: [{ is_adm: { $exists: false } }, { is_adm: false }] })
+          .select('name nickname email img is_adm is_premium is_banned');
+      } else {
+        recentUsers = await UserModel.find({ $or: [{ is_adm: { $exists: false } }, { is_adm: false }] })
           .sort({ createdAt: -1 })
-          .select('name nickname email img is_adm is_premium is_banned'); 
+          .select('name nickname email img is_adm is_premium is_banned');
       }
 
       return recentUsers;
@@ -390,48 +390,57 @@ class User{
     }
   }
 
-  async usersTotal(){
+  async usersTotal() {
     try {
       const summary = await UserModel.aggregate([
         {
           $facet: {
             totalUsers: [{ $count: "count" }],
             premiumUsers: [{ $match: { is_premium: true } }, { $count: "count" }],
+            nonPremiumUsers: [{ $match: { is_premium: false } }, { $count: "count" }],
             bannedUsers: [{ $match: { is_banned: true } }, { $count: "count" }],
-            notBannedUsers: [{ $match: { is_banned: false } }, { $count: "count" }]
+            notBannedUsers: [{ $match: { is_banned: false } }, { $count: "count" }],
+            adminUsers: [{ $match: { is_adm: true } }, { $count: "count" }],
+            commonUsers: [{ $match: { is_adm: false } }, { $count: "count" }]
           }
         },
         {
           $project: {
             totalUsers: { $arrayElemAt: ["$totalUsers.count", 0] },
             premiumUsers: { $arrayElemAt: ["$premiumUsers.count", 0] },
+            nonPremiumUsers: { $arrayElemAt: ["$nonPremiumUsers.count", 0] },
             bannedUsers: { $arrayElemAt: ["$bannedUsers.count", 0] },
-            notBannedUsers: { $arrayElemAt: ["$notBannedUsers.count", 0] }
+            notBannedUsers: { $arrayElemAt: ["$notBannedUsers.count", 0] },
+            adminUsers: { $arrayElemAt: ["$adminUsers.count", 0] },
+            commonUsers: { $arrayElemAt: ["$commonUsers.count", 0] }
           }
         }
       ]);
-  
+
       return summary[0] || {
         totalUsers: 0,
         premiumUsers: 0,
+        nonPremiumUsers: 0,
         bannedUsers: 0,
-        notBannedUsers: 0
+        notBannedUsers: 0,
+        adminUsers: 0,
+        commonUsers: 0
       };
     } catch (error) {
       console.log("Erro ao buscar quantidade de usuarios", error);
       throw error;
     }
   };
-  
 
-  async getUsersADM(){
-    try{
+
+  async getUsersADM() {
+    try {
       const users = await UserModel.find({})
-      .sort({ createdAt: -1 })
-      .select('name nickname email img is_adm is_premium is_banned');
+        .sort({ createdAt: -1 })
+        .select('name nickname email img is_adm is_premium is_banned');
 
       return users;
-    }catch(err){
+    } catch (err) {
       console.log(err);
       this.errors.push('Não foi possivel buscar usuários.');
       return;
